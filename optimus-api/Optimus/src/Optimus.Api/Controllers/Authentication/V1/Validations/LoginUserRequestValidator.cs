03b7d697c5d6407290f400f9171db25e86286d94
Carlos Junior <carlosbritojun@gmail.com>
2024-05-21 02:29:53 -0300

    [backend] Melhoria nas mensagens do login

--- optimus-api/Optimus/src/Optimus.Api/Controllers/Authentication/V1/Validations/LoginUserRequestValidator.cs
+++ optimus-api/Optimus/src/Optimus.Api/Controllers/Authentication/V1/Validations/LoginUserRequestValidator.cs
@@ -8,13 +8,13 @@ public sealed class LoginUserRequestValidator : AbstractValidator<LoginUserReque
     public LoginUserRequestValidator()
     {
         RuleFor(user => user.Email)
-            .NotNull()
-            .NotEmpty()
-            .EmailAddress();
+            .NotNull().WithMessage("O e-mail deve ser informado")
+            .NotEmpty().WithMessage("O e-mail não deve ser branco")
+            .EmailAddress().WithMessage("O e-mail informado é inválido");
 
         RuleFor(user => user.Password)
-            .NotNull()
-            .NotEmpty()
-            .Length(4, 10);
+            .NotNull().WithMessage("O password deve ser informado")
+            .NotEmpty().WithMessage("O password não deve ser branco")
+            .Length(4, 10).WithMessage("O password deve ter entre 4 e 10 caracteres");
     }
 }
